2558096c72ee4d8a8f4caecc6eb3ea9697baa22f
RicardoPayan <a218206581@unison.mx>
2022-09-20 20:47:59 -0700

    Trabajando en autenticacion

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,13 @@
+
+const checkAuth = (req, res, next) =>{
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')){
+        console.log('Si tiene el token con bearer');
+    }
+    
+
+    const error = new Error('token no valido o inexistente');
+    res.status(403).json({msg : error.message});
+    next();
+}
+
+export default checkAuth;
--- backend/routes/veterinarioRoutes.js
+++ backend/routes/veterinarioRoutes.js
@@ -4,12 +4,20 @@ import { registrar,
     perfil, 
     confirmar, 
     autenticar } from '../controllers/veterinarioController.js';
+import checkAuth from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
+
+//Rutas para area publica (no se necesita cuenta para acceder a ellas)
 router.post("/",registrar);
-router.get('/perfil',perfil);
 router.get('/confirmar/:token', confirmar);
 router.post('/login',autenticar);
+
+//En esta rutas primero verificamos que el usuario este autentificado
+// si lo esta, entonces continua el codigo.
+router.get('/perfil',checkAuth,perfil);
+
+
 
 export default router;
