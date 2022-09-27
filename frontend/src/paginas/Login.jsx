6f53358ba9a87d8914fa590301a098cd8cc0620f
RicardoPayan <a218206581@unison.mx>
2022-09-26 20:59:56 -0700

    Trabajando en el frontend de Login

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -2,7 +2,45 @@
 const Login = () => {
   return (
     <>
-        <h2>Login</h2>
+      
+      <div>
+          <h1 className="text-indigo-600 font-black text-6xl">
+            Inicia Sesión y Administra tus <span className="text-black">Pacientes</span>
+          </h1>
+      </div>
+      <div>
+          <form>
+            <div className="my-5">
+              <label className="uppercase text-gray-600 block text-xl font-bold">
+                Email
+              </label>
+              <input
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl "
+                type="email"
+                placeholder="Email de registro"
+              />
+            </div>
+
+            <div className="my-5">
+              <label className="uppercase text-gray-600 block text-xl font-bold">
+                Contraseña
+              </label>
+              <input
+                className="border w-full p-3 mt-3 bg-gray-50 rounded-xl "
+                type="password"
+                placeholder="Contraseña"
+              />
+            </div>
+
+            <input 
+              className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+              type="submit" 
+              value="Iniciar Sesión"
+            />
+          </form>
+      </div>
+    
+        
     </>
   )
 }
