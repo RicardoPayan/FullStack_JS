ac8bcb76a6ed79bff59e247955d7f4b02aa495e8
RicardoPayan <a218206581@unison.mx>
2022-10-20 21:31:06 -0700

    Pagina para nuevo password

--- frontend/src/paginas/nuevoPassword.jsx
+++ frontend/src/paginas/nuevoPassword.jsx
@@ -1,8 +1,83 @@
+import {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
+import Alerta from "../components/Alerta.jsx";
+import clienteAxios from "../config/axios.jsx";
 
 
 const NuevoPassword = () => {
+
+  const [password, setPassword] = useState('');
+  const [alerta, setAlerta] = useState({})
+  const [tokenValido, setTokenValido] = useState(false);
+
+  const params = useParams()
+  const {token} = params;
+
+  useEffect(() =>{
+    const comprobarToken = async () =>{
+      try {
+        await clienteAxios(`/veterinarios/olvide-password/${token}`);
+
+        setAlerta({
+          msg : 'Coloca tu nuevo password'
+        })
+
+        setTokenValido(true);
+
+      } catch (error) {
+        setAlerta({
+          msg : 'Hubo un error con el enlace',
+          error : true
+        })
+      }
+    }
+
+    comprobarToken();
+  }, []);
+
+  const {msg} = alerta;
+
   return(
-      <h1>Desde nuevo password</h1>
+      <>
+        <div>
+          <h1 className="text-indigo-600 font-black text-6xl">
+            Reestablece tu Contraseña y no Pierdas
+            Acceso a <span className="text-black">tus Pacientes</span>
+          </h1>
+        </div>
+
+        <div className="mt-20 md:mt-5 shadow-lg px-5 py-10 rounded-xl bg-white">
+          {msg && <Alerta
+              alerta = {alerta}
+          />}
+
+          {/*En caso de ser un token valido, entonces devolvemos el forms*/}
+          {tokenValido && (
+              <form>
+                <div className="my-5">
+                  <label className="uppercase text-gray-600 block text-xl font-bold">
+                    Nueva Contraseña
+                  </label>
+                  <input
+                      className="border w-full p-3 mt-3 bg-gray-50 rounded-xl "
+                      type="password"
+                      placeholder="Contraseña"
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                  />
+                </div>
+
+                <input
+                    className="bg-indigo-700 w-full py-3 px-10 rounded-xl text-white uppercase font-bold mt-5 hover:cursor-pointer hover:bg-indigo-800 md:w-auto"
+                    type="submit"
+                    value="Reestablecer Contraseña"
+                />
+              </form>
+
+          )}
+
+        </div>
+      </>
   )
 }
 
